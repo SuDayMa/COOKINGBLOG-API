afb3cabc0faa6b309f4082d29f715c52771025e9
Quoc Su <dragongamingtv2k5@gmail.com>
2025-12-31 10:35:35 +0700

    Create Comment.js

---
+++ models/Comment.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+const commentSchema = new mongoose.Schema(
+  {
+    id: { type: String, default: uuidv4, unique: true, index: true },
+    post_id: { type: String, required: true, index: true },
+    user_id: { type: String, required: true, index: true },
+    content: { type: String, required: true },
+  },
+  { timestamps: { createdAt: "created_at", updatedAt: false } }
+);
+
+module.exports = mongoose.model("Comment", commentSchema);
