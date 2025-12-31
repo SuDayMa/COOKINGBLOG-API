fc95f05abf9426ba89658ba2b58d42cebacd232b
Quoc Su <dragongamingtv2k5@gmail.com>
2025-12-31 11:09:17 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -7,9 +7,13 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true },
+
     avatar: { type: String, default: null },
     phone: { type: String, default: null },
     bio: { type: String, default: null },
+
+    role: { type: String, enum: ["user", "admin"], default: "user", index: true },
+    is_blocked: { type: Boolean, default: false, index: true },
   },
   { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
