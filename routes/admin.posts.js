dd9ef921648b850d5a4a2af280b41933682b9353
Quoc Su <dragongamingtv2k5@gmail.com>
2026-01-08 20:02:48 +0700

    fix admin routes

--- routes/admin.posts.js
+++ routes/admin.posts.js
@@ -2,9 +2,17 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/admin/postController");
 
+console.log("DANH SÁCH HÀM TRONG CONTROLLER:", Object.keys(postController));
+
+if (!postController.getAdminPosts) console.error("LỖI: Thiếu hàm getAdminPosts");
+if (!postController.updatePostStatus) console.error("LỖI: Thiếu hàm updatePostStatus");
+if (!postController.deletePost) console.error("LỖI: Thiếu hàm deletePost");
 
 router.get("/", postController.getAdminPosts);
-router.patch("/:id/status", postController.updatePostStatus);
-router.delete("/:id", postController.deletePost);
-
+if (postController.updatePostStatus) {
+    router.patch("/:id/status", postController.updatePostStatus);
+}
+if (postController.deletePost) {
+    router.delete("/:id", postController.deletePost);
+}
 module.exports = router;
