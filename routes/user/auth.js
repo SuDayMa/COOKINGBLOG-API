0eea2e68111b78bf3f62a89fc61da095cdaa638d
Quoc Su <dragongamingtv2k5@gmail.com>
2026-01-23 21:50:30 +0700

    fix auth

--- routes/user/auth.js
+++ routes/user/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controllers/authController");
-const auth = require("../middleware/auth");
+
+const authController = require("../../controllers/authController"); 
+const auth = require("../../middleware/auth"); 
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
