eee3be4e6f643876e4c774aa0f82f1cf3d338b43
Quoc Su <dragongamingtv2k5@gmail.com>
2026-01-23 22:10:19 +0700

    fix users.js

--- routes/user/users.js
+++ routes/user/users.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../middleware/upload");
-const auth = require("../middleware/auth");
-const userController = require("../controllers/userController");
+
+const upload = require("../../utils/fileUpload"); 
+const auth = require("../../middleware/auth"); 
+const userController = require("../../controllers/userController");
 
 router.get("/:id", userController.getPublicProfile);
 router.get("/:id/posts", userController.getUserPosts);
 router.put("/profile", auth, userController.updateProfile);
 router.patch("/update-avatar", auth, upload.single("avatar"), userController.updateAvatar);
+
 module.exports = router;
