9edb544c747fb27263cf2a8dc9321a4fdc98d16e
Quoc Su <dragongamingtv2k5@gmail.com>
2026-02-19 22:14:56 +0700

    fix post

--- routes/user/posts.js
+++ routes/user/posts.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
-const postController = require("../controllers/postController"); 
-const multer = require("multer");
 
-const upload = multer({ dest: "uploads/" }); 
+const auth = require("../../middleware/auth");
+const postController = require("../../controllers/postController"); 
+
+const upload = require("../../utils/fileUpload");
+
 router.get("/me", auth, postController.getMyPosts);
+
 router.route("/")
   .get(postController.getPosts) 
-  .post(auth, upload.single("image"), postController.createPost);
+  .post(
+    auth, 
+    upload.fields([
+      { name: "images", maxCount: 10 }, 
+      { name: "video", maxCount: 1 }    
+    ]), 
+    postController.createPost
+  );
 
 module.exports = router;
