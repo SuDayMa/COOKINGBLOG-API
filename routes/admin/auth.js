265817da10db4dc88c5a0193f0475918995cc59c
Quoc Su <dragongamingtv2k5@gmail.com>
2026-02-19 22:44:02 +0700

    fix khoang cach auth

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controllers/admin/authController");
+const authController = require("../../controllers/admin/authController");
 
 // POST /api/admin/login
 router.post("/login", authController.login);
