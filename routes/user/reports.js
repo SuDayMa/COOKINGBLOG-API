1f6efc70500b4f0abc9b723f554c36e5821335a2
Quoc Su <dragongamingtv2k5@gmail.com>
2026-02-19 22:19:52 +0700

    fix ../

--- routes/user/reports.js
+++ routes/user/reports.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
+const auth = require("../../middleware/auth");
 const reportController = require("../controllers/reportController");
 
 router.post("/", auth, reportController.createReport);
