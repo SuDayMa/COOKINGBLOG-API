4553529c7b7007af51526cf21dcd000f80943356
Quoc Su <dragongamingtv2k5@gmail.com>
2026-02-23 22:50:48 +0700

    update user\auth

--- routes/user/auth.js
+++ routes/user/auth.js
@@ -5,7 +5,13 @@ const authController = require("../../controllers/authController");
 const auth = require("../../middleware/auth"); 
 
 router.post("/register", authController.register);
+
+router.post("/verify-otp", authController.verifyOtp); 
+
+// Đăng nhập
 router.post("/login", authController.login);
+
+// Lấy thông tin cá nhân
 router.get("/me", auth, authController.getMe);
 
 module.exports = router;
